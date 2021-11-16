7c5cfd31275b98fa70dfd11efae0338485d934ae
Gypopo <ferreschillemans@gmail.com>
2021-11-16 15:20:20 +0100

    1.0.1
    - Fixed NPE while getting a items price
    - Added a method to get the sell price with checking the required permissions of the given player

--- src/main/java/me/gypopo/economyshopgui/api/EconomyShopGUIHook.java
+++ src/main/java/me/gypopo/economyshopgui/api/EconomyShopGUIHook.java
@@ -10,6 +10,7 @@ public class EconomyShopGUIHook {
     /**
      * Gets a item sell price
      * If the item couldn't be found inside the shop, this will return null
+     * Note: This method does NOT check for player permissions, instead you should use {@link #getItemSellPrice(Player, ItemStack)}
      *
      * @param item The item to get the sell price for
      * @return The sell price of the item, if not found this will return null
@@ -19,8 +20,22 @@ public class EconomyShopGUIHook {
     }
 
     /**
+     * Gets a item sell price
+     * If the item couldn't be found inside the shop, this will return null
+     * Note: This method DOES check if the player has the required permissions to sell items in the required shop category
+     *
+     * @param player Player to check for permissions
+     * @param item The item to get the sell price for
+     * @return The sell price of the item, if not found this will return null
+     */
+    public static Double getItemSellPrice(Player player, ItemStack item) {
+        return null;
+    }
+
+    /**
      * Gets a item buy price
      * If the item couldn't be found inside the shop, this will return null
+     * Note: This method does NOT check for player permissions, instead you should use {@link #getItemBuyPrice(Player, ItemStack)}
      *
      * @param item The item to get the buy price for
      * @return The buy price of the item, if not found this will return null
@@ -32,8 +47,9 @@ public class EconomyShopGUIHook {
     /**
      * Gets a item buy price with any discount included for the player if present
      * If the item couldn't be found inside the shop, this will return null
+     * Note: This method DOES check if the player has the required permissions to buy items in the required shop category
      *
-     * @param player Player to check for discounts
+     * @param player Player to check for discounts and permissions
      * @param item The item to get the buy price for
      * @return The buy price of the item, if not found this will return null
      */
