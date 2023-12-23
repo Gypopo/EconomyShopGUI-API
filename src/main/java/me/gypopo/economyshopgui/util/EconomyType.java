6f27f80a5baba9c5f3ed525636a1cfd39840faf6
BuildTools <ferreschillemans@gmail.com>
2023-12-24 00:53:13 +0100

    Added EcoBits and CoinsEngine as EconomyType

--- src/main/java/me/gypopo/economyshopgui/util/EconomyType.java
+++ src/main/java/me/gypopo/economyshopgui/util/EconomyType.java
@@ -3,9 +3,11 @@ package me.gypopo.economyshopgui.util;
 public enum EconomyType {
 
     EXP("EXP"),
-    LEVELS("Levels"),
     ITEM("Item"),
     VAULT("Vault"),
+    LEVELS("Levels"),
+    ECOBITS("EcoBits"),
+    COINS_ENGINE("CoinsEngine"),
     PLAYER_POINTS("PlayerPoints"),
     GEMS_ECONOMY("GemsEconomy"),
     ULTRA_ECONOMY("UltraEconomy"),
